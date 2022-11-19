17bdba3db801cabfb8250a05a0e6040b25130f05
Alex <alexsa-8@mail.ru>
2022-11-19 20:21:01 +0300

    карзина интернет магазина

---
+++ src/main/java/com/example/cartonlinestore/CartOnlineStoreApplication.java
@@ -0,0 +1,13 @@
+package com.example.cartonlinestore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CartOnlineStoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CartOnlineStoreApplication.class, args);
+    }
+
+}
